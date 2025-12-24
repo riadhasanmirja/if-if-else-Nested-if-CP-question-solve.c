af7f727428709117678cda36ee2ce8662d50cc15
MD. Riad Hasan Mirja <riadrikto0357@gmail.com>
2025-12-24 19:43:09 +0600

    Create Positive Negative.c

---
+++ Positive Negative.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    
+    int a;
+    
+    printf("Enter The Number :");
+    scanf("%d",&a);
+    
+    if(a>=0)
+    printf("Positive");
+    
+    else
+    printf("Negative");
+    
+    return 0;
+}

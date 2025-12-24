bedcff003bc488bbb2572cf5b42e10e4e818f956
MD. Riad Hasan Mirja <riadrikto0357@gmail.com>
2025-12-24 19:53:24 +0600

    Create Pass Fail.c

---
+++ Pass Fail.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    
+    int Marks;
+    
+    printf("Enter The Number :");
+    scanf("%d",&Marks);
+    
+    if(Marks>=33)
+    printf("Pass");
+    
+    else
+    printf("Fail");
+    
+    return 0;
+}

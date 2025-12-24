f876d1d7dd26d126762a64867dad2a63341914ec
MD. Riad Hasan Mirja <riadrikto0357@gmail.com>
2025-12-24 19:50:44 +0600

    Create Even Odd.c

---
+++ Even Odd.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    
+    int a;
+    
+    printf("Enter The Number :");
+    scanf("%d",&a);
+    
+    if(a%2==0)
+    printf("Even");
+    
+    else
+    printf("Odd");
+    
+    return 0;
+}
